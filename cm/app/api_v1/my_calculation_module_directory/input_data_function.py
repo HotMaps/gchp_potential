ee878132257a562d518da4a43d27997e1f23964c
pietro <peter.zamb@gmail.com>
2019-11-20 17:57:32 +0100

    Move the GRASS GIS data directory from user home to a temporary directory

--- cm/app/api_v1/my_calculation_module_directory/input_data_function.py
+++ cm/app/api_v1/my_calculation_module_directory/input_data_function.py
@@ -9,8 +9,10 @@ Created on Mon May 27 11:37:50 2019
  gcore.run_command("v.import", input="/tmp/hotmaps/cm_files_uploaded/thermomap.shp", layer="termomap",  output="d")
 """
 import os
+import tempfile
 #INPUT_CALCULATION_MODULE = [{"path" : "/home/gis_db"
 #                            }]
 
 
-PATH = os.path.join(os.getenv("HOME"), "gis_db")
+PATH = os.path.join(tempfile.gettempdir(), "gis_db")
+os.makedirs(PATH, exist_ok=True)
